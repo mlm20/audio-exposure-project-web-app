fac4617b0c79e1381672d2e84a316c311e28ceb0
Maximilian Matthews <maxmatthews2048@gmail.com>
2023-11-27 14:52:55 +0000

    major changes

---
+++ server.js
@@ -0,0 +1,21 @@
+// App setup
+const app = require("./app");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Misc imports
+// import Handler from "handler.js";
+
+
+// app.post("/", function (req, res) {
+//     const request_object = req.body;
+
+//     Handler[request_object.type](request_object).then(function (
+//         response_object
+//     ) {
+//         res.json(response_object);
+//     });
+// });
