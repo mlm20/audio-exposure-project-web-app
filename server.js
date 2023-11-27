d25abc5a3eea79eb0d8a4d3ce92ece6f9fbb6dfc
Maximilian Matthews <maxmatthews2048@gmail.com>
2023-11-27 15:07:09 +0000

    enabled Handler

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ app.listen(port, () => {
 });
 
 // Misc imports
-// import Handler from "handler.js";
+import Handler from "handler.js";
 
+app.post("/", function (req, res) {
+    const request_object = req.body;
 
-// app.post("/", function (req, res) {
-//     const request_object = req.body;
-
-//     Handler[request_object.type](request_object).then(function (
-//         response_object
-//     ) {
-//         res.json(response_object);
-//     });
-// });
+    Handler[request_object.type](request_object).then(function (
+        response_object
+    ) {
+        res.json(response_object);
+    });
+});
